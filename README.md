13eac243e8f2461fcd2a2287810d3c1e66ad97f8
Amy Zhou <zhouamy77@gmail.com>
2018-07-24 20:37:29 +0000

    added README file

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Project 2
 
+Amy Zhou
 Web Programming with Python and JavaScript
+Harvard Secondary School Program
+
+Chatterbox is a messaging website that implements Python and Javascript together.
+Socket.IO and AJAX were used for client side and server side communication, as well
+as asynchronous requests. There are bootstrap css styling components as well.
+
+This single-page application is an online messaging service using Flask.
+Users are prompted to type in a display name, which is stored in LocalStorage.
+After a username exists in LocalStorage, a the innerHTML dynamically changes, welcoming
+the user to the website.
+
+Users may either enter an existing channel or create a new channel. When the user enters
+the name of a new channel, the name of the new channel is appended to a dropdown menu,
+featuring the entire list of available channels.
+
+Users can navigate to different channels and chat in different groups. They can see and
+join existing channels to send and receive messages with one another in real time.
+
+To send a text, type a message in the text box in the body area of the Chatterbox website.
+Click the Send button to send the message, which will be appended to a list of all messages
+in server side storage. The new message will be displayed below the text area, showing the
+message, user who sent the text, datetime, and the current message in which the message
+is sent.
+
+I used Bootstrap to style my website. The components include jumbotron header and columns
+for the left-side and body panels. I used css properties to innovate the design, espcially
+in the color scheme and layout of the website.
--- static/index.js
+++ static/index.js
@@ -27,109 +27,77 @@ function uploadchannmasg()
 }
 
 
+// keep listening for action from user, such as pressing a button
 document.addEventListener('DOMContentLoaded', () =>
 {
    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+   // when the Send button is clicked, implement the following section of code
    socket.on('connect', () => {
        document.querySelectorAll('#btnsendmsg').forEach(button => {
             button.onclick = () => {
+               // define variables for the datetime, display name, message, and current channel name
                var d = new Date();
                var showname = localStorage.getItem('username');
                var sentmsg = document.querySelector('#txtmessage').value;
                var currchan = document.querySelector('#lstchannel').value;
-            //   document.getElementById("msgbox").innerHTML = d.toString();
-               alert("New Message: " + sentmsg + " - " + showname);
+               // socket emit the message, datetime, username, and channel name
                socket.emit('submit message', {"text" : sentmsg, "date" : d.toString(), "user" : showname, "lstchannel" : currchan});
             };
         });
+        // when the Create channel button is clicked, implement the following code
         document.querySelectorAll('#createchannel').forEach(button => {
             button.onclick = () => {
-                // alert(channel_list);
-                var existchannels = channel_list;
-                var channelname=document.getElementById("txtchannel").value;
+                var channelname = document.getElementById("txtchannel").value;
                 var select = document.getElementById("lstchannel");
                 var option = document.createElement("option");
-                for (var i=0; i<channel_list.length; i++)
+                // iterate through the list of channels
+                for (var i = 0; i < channel_list.length; i++)
                 {
-                    if (channelname==channel_list[i])
+                    // if the input channel name already exists in the list, prompt the user to try again and input a new name
+                    if (channelname == channel_list[i])
                     {
                         return(alert("This channel already exists. Please input a new name."));
                     }
                 }
-                // alert(channelname);
+                // add push the new channel name to the list of channels
                 channel_list.push(channelname);
                 option.text = channelname;
                 option.value = channelname;
-                // alert(channelname);
                 select.add(option);
                 // select.insertAdjacentHTML('beforeEND',select.join('\n'));
-                alert(select.value);
+                // socket emit, reference server side in application.py for a list of channels
                 socket.emit('existchann', {"lstchannel" : select});
             };
         });
     });
 
+    // socket receive the messages in the dictionary and display the message on the screen
     socket.on('receive', data => {
+        // initialize the empty allchann array
         allchann = []
-        alert(allchann);
+        // if the number of items in the allchann array does not exceed 100, append to the array the message and corresponding credentials
         for (i = 0; i < 1; i++) {
-            if (allchann.length < 3) {
+            if (allchann.length < 2) {
                 const li = document.createElement('li');
                 document.querySelector('#msgbox').append(li);
                 data["date"];
+                // in a list, display from the data dictionary the name of the user who sent the message, the text message, the datetime sent, and the current channel
                 li.innerHTML = `${data["user"]} : ${data["text"]} - ${data["date"]} - ${data["lstchannel"]}`;
                 li.appendChild(allchann);
                 document.getElementById("msgbox").appendChild(li);
             }
+            // if the length of the array is greater than 100, pop the earliest message
             else {
                 allchann.pop();
                 alert(allchann);
             }
         }
     });
-
+    // socket server side application.py
     socket.on('here', chann => {
-        alert("socketio works here");
+        // append the input channel names to the list in the dropdown menu
         const lichann = document.createElement('lichann');
         document.querySelector('#lstchannel').append(lichann);
     });
 });
 
-
-
-
-// document.addEventListener('DOMContentLoaded', () =>
-// {
-//     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-//     socket.on('connect', () => {
-//         document.querySelectorAll('#createchannel').forEach(button => {
-//             button.onclick = () => {
-//                 alert(channel_list);
-//                 var existchannels = channel_list;
-//                 var channelname=document.getElementById("txtchannel").value;
-//                 var select = document.getElementById("lstchannel");
-//                 var option = document.createElement("option");
-//                 for (var i=0; i<channel_list.length; i++)
-//                 {
-//                     if (channelname==channel_list[i])
-//                     {
-//                         return(alert("This channel already exists. Please input a new name."));
-//                     }
-//                 }
-//                 alert(channelname);
-//                 channel_list.push(channelname);
-//                 option.text = channelname;
-//                 option.value = channelname;
-//                 select.add(option);
-//                 select.insertAdjacentHTML('beforeEND',select.join('\n'));
-//                 socket.emit('existchann', {"lstchannel" : currchan});
-
-//             };
-//         });
-//     });
-//     socket.on('here', chann => {
-//         alert("socketio works here");
-//         const lichann = document.createElement('lichann');
-//         document.querySelector('#lstchannel').append(lichann);
-//     });
-// });
